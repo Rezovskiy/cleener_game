a9f7170ef47a53fbdb6af26257de04401f8d67a0
Bastaf24 <dasa.farsh@bk.ru>
2026-06-13 17:44:49 +0300

    Фикс ошибки когда после нажатия кнопки 'play' не запускалась игра

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 
 func _on_button_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Level_scenes/level_1.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Level_scenes/level_1-1.tscn")
 
 
 func _on_button_quit_pressed() -> void:
