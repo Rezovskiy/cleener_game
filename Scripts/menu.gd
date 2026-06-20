b9aa2132f19b03ec26546ed3b780301a7314673f
Bastaf24 <dasa.farsh@bk.ru>
2026-06-20 17:48:12 +0300

    Движение камеры вместе с ходьбой камеры
    - Камера двигается при ходьбе чуть дальше игрока для удобства
    - Если игрок стоит на месте 2.5 секунды камера возращается в центр экрана (не плавно)

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 
 
+
 func _on_button_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Level_scenes/level_1-1.tscn")
 
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,10 +1,9 @@
 extends CharacterBody2D
-
+@onready var camera_2d = $Camera2D
+@onready var return_camera_timer = $ReturnCameraTimer
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
-
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -17,9 +16,21 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	
 	if direction:
+		
+		if direction > 0 and camera_2d.offset.x < 31:
+			camera_2d.offset.x += 2
+		elif camera_2d.offset.x > - 31:
+			camera_2d.offset.x -= 2
+		return_camera_timer.start()
 		velocity.x = direction * SPEED
+	
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
+
+
+func _on_return_camera_timer_timeout():
+	camera_2d.offset.x = 0
